9f4cfac68c27e2a843463b2a2c62a67431b7b379
Felicitas Yeboah <felicitas.yeboah@stud.th-luebeck.de>
2022-01-15 22:08:34 +0100

    added attributes font, done, next, previous

--- ts/views/State.ts
+++ ts/views/State.ts
@@ -5,12 +5,6 @@ import {Canvasdata} from "../main.js";
  * Die Abstrakte Klasse State wird von jeder View implementiert. Sie stellt die Views dar.
  */
 export abstract class State {
-    //TODO: FRAGE: soll ein statisches Attribut sein, oder besser im Constructo???
-    public static KEY = {
-        LEFT: ['ArrowLeft', 'a'],
-        RIGHT: ['ArrowRight', 'd'],
-        JUMP: [' ', 'w', 'ArrowUp']
-    }
     protected bufferCanvas = this.CANVAS_DATA.BUFFER_CANVAS;
     protected bufferCtx = this.CANVAS_DATA.BUFFER_CTX;
     protected displayCanvas = this.CANVAS_DATA.DISPLAY_CANVAS;
@@ -20,27 +14,48 @@ export abstract class State {
     protected mapRows = this.CANVAS_DATA.ROWS;
     protected mapCols = this.CANVAS_DATA.COLS;
     protected tileSize = this.CANVAS_DATA.TILE_SIZE;
+    protected font = this.CANVAS_DATA.FONT;
+    private _done: boolean;
+    private _next: string;
+    private _previous: string;
 
     protected constructor(readonly CANVAS_DATA: Canvasdata) {
-        // if (this.constructor === State) {
-        //     throw new Error("Abstract classes can't be instantiated.");
-        // }
+        this._done = false;
+        this._next = "";
+        this._previous = "";
     }
 
     //Abstrake Methode
     abstract startup(): void; //  { throw new Error("Method 'startup()' must be implemented.");}
 
     //Abstrake Methode
-    cleanup() {
-        throw new Error("Method 'cleanup()' must be implemented.");
-    }
+    abstract cleanup(): void; //throw new Error("Method 'cleanup()' must be implemented.");
 
     //Abstrake Methode
-    abstract getEvent(event: { [key: string]: string }): void; // { throw new Error("Method 'getEvent()' must be implemented.");}
+    abstract update(): void; // throw new Error("Method 'update()' must be implemented.");
 
-    //Abstrake Methode
-    update() {
-        throw new Error("Method 'update()' must be implemented.");
+
+    get done(): boolean {
+        return this._done;
     }
 
+    set done(value: boolean) {
+        this._done = value;
+    }
+
+    get next(): string {
+        return this._next;
+    }
+
+    set next(value: string) {
+        this._next = value;
+    }
+
+    get previous(): string {
+        return this._previous;
+    }
+
+    set previous(value: string) {
+        this._previous = value;
+    }
 }
